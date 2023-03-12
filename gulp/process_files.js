6c35d42908761b917a48545c9032c59f18a27cef
Felipe Matheus Flohr <felipeflohrlol@gmail.com>
2023-03-12 03:05:58 -0300

    CI CD setup

---
+++ gulp/process_files.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const gulp = require("gulp");
+
+function processJson() {
+    return gulp
+        .src("lib/**/*.json", { allowEmpty: true })
+        .pipe(gulp.dest("dist"));
+}
+
+function processPackageFiles() {
+    return gulp
+        .src(["package.json", "LICENSE", "README.md", ".npmrc"], { allowEmpty: true })
+        .pipe(gulp.dest("dist"));
+}
+
+module.exports = {
+    processJson,
+    processPackageFiles,
+};
